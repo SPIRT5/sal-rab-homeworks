da960a26cb95511b24c4d2659e0e606911fd3a1e
SPIRT5 <108623559+SPIRT5@users.noreply.github.com>
2022-12-16 12:28:36 +0300

    Add files via upload

---
+++ task_4/solution.js
@@ -0,0 +1,19 @@
+// Задание 4.1. Вывод карточек товаров
+
+function parseProducts(json) {
+    // Аргументом функции является JSON
+
+    let data = JSON.parse(json);
+    return data.products;
+}
+
+function renderProductsCards(json) {
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i <= length - 1; i++) { // выведет 0, затем 1, затем 2
+        addProduct(products[i]);
+    }    
+}
+
+// Конец решения задания №4.1.
